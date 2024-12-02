5fe9f3021cadbfa29ce87b34664a912d59a68f73
Yaoxin Xiang <28193915+xiangyx@users.noreply.github.com>
2024-12-02 23:08:35 +0100

    day02

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ### Day 01
 - $\ell_1$ distance between two sorted lists
 - hashmap lookup
+
+### Day 02
+- smooth strict monotonic function
+- tolerate a single bad level -> l[:i] + l[i+1:]
---
+++ sol/02.py
@@ -0,0 +1,32 @@
+def is_strictly_increasing_or_decreasing(report):
+    diff = [y - x for x, y in zip(report[:-1], report[1:])]
+    is_increasing = all(1 <= d and d <= 3 for d in diff)
+    is_decreasing = all(-3 <= d and d <= -1 for d in diff)
+    return is_increasing or is_decreasing
+
+def is_report_safe(report):
+    if is_strictly_increasing_or_decreasing(report):
+        return True
+    for i in range(len(report)):
+        sub_report = report[:i] + report[i+1:]
+        if is_strictly_increasing_or_decreasing(sub_report):
+            return True
+            
+def main():
+    Q1_safe_reports = 0
+    Q2_safe_reports = 0
+    with open("input/02.txt", "r") as reports:
+        for report in reports:
+            report = report.split()
+            report = [int(level) for level in report]
+            if is_strictly_increasing_or_decreasing(report):
+                Q1_safe_reports += 1
+            if is_report_safe(report):
+                print(f"Safe report: {report}")
+                Q2_safe_reports += 1
+    print(f"answer to Q1: {Q1_safe_reports}")
+    print(f"answer to Q2: {Q2_safe_reports}")
+
+if __name__ == '__main__':
+    main()
+    
